defd0f63ea944749fb880a23cb583b6f9246e610
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-29 00:17:12 +0300

    Добавьте файлы проекта.

---
+++ FinalTask/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalTask
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
